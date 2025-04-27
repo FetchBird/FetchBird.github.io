680687b0ed54308622c6e0852c6f2d194bbc4e2f
feeribeiro <fee.ribeiro.ribeiro96@gmail.com>
2025-04-27 12:03:45 -0300

    [UPDATE] formulario de contato e media queries

--- public/js/modules/contactForm.js
+++ public/js/modules/contactForm.js
@@ -1,29 +1,94 @@
-export default function contactForm() {
+function moveSlider(radio, slider) {
+    const whatsappIcon = document.querySelector('.whatsapp-icon-container');
+    const emailIcon = document.querySelector('.email-icon-container');
+
+    if (radio.value == 'email') {
+        slider.classList.remove('right');
+
+        whatsappIcon.querySelector('.whatsapp-icon-black').classList.add('active');
+        whatsappIcon.querySelector('.whatsapp-icon-orange').classList.remove('active');
+
+        emailIcon.querySelector('.email-icon-black').classList.remove('active');
+        emailIcon.querySelector('.email-icon-orange').classList.add('active');
+    } else {
+        slider.classList.add('right');
+
+        whatsappIcon.querySelector('.whatsapp-icon-black').classList.remove('active');
+        whatsappIcon.querySelector('.whatsapp-icon-orange').classList.add('active');
+
+        emailIcon.querySelector('.email-icon-black').classList.add('active');
+        emailIcon.querySelector('.email-icon-orange').classList.remove('active');
+
+    }
+}
+
+function initContactMethod(radios = [], slider) {
+    const radioChecked = Array.from(radios).find(x => x.checked);
+
+    if (!radioChecked) return;
+
+    moveSlider(radioChecked, slider);
+}
+
+function toggleContactMethod() {
     const radios = document.querySelectorAll('input[name="contact"]');
     const slider = document.querySelector('.slider');
-    const whatsappIcon  = document.querySelector('.whatsapp-icon-container');
-    const emailIcon  = document.querySelector('.email-icon-container');
+
+    initContactMethod(radios, slider)
 
     radios.forEach((radio, index) => {
         radio.addEventListener('change', () => {
-            if (index == 0) {
-                slider.classList.toggle('right');
-                
-                whatsappIcon.querySelector('.whatsapp-icon-black').classList.toggle('active');
-                whatsappIcon.querySelector('.whatsapp-icon-orange').classList.remove('active');
-                
-                emailIcon.querySelector('.email-icon-black').classList.remove('active');
-                emailIcon.querySelector('.email-icon-orange').classList.add('active');
-            } else {
-                slider.classList.toggle('right');
-                
-                whatsappIcon.querySelector('.whatsapp-icon-black').classList.remove('active');
-                whatsappIcon.querySelector('.whatsapp-icon-orange').classList.add('active');
-
-                emailIcon.querySelector('.email-icon-black').classList.add('active');
-                emailIcon.querySelector('.email-icon-orange').classList.remove('active');
-                
-            }
+            moveSlider(radio, slider)
         });
     });
 };
+
+function toggleBudgetInput() {
+    const budget = document.getElementById('budget-container');
+    const select = document.getElementById('subject');
+    if (!select) return;
+
+    select.addEventListener('change', () => {
+        const value = select.value;
+        if (value === 'orcamento') {
+            budget.classList.remove('hidden');
+        } else {
+            budget.classList.add('hidden');
+        }
+    });
+}
+
+function initiContactInfoLabel(radios, contactInfoLabel) {
+    const radioChecked = Array.from(radios).find(x => x.checked);
+
+    if (!radioChecked) return;
+
+    if (radioChecked.value === 'email') {
+        contactInfoLabel.innerText = 'Email';
+    } else if (radioChecked.value === 'whatsapp') {
+        contactInfoLabel.innerText = 'WhatsApp';
+    }
+}
+
+function toggleContactInfoLabel() {
+    const radios = document.querySelectorAll('input[name="contact"]');
+    const contactInfoLabel = document.querySelector('#contact-info-container > label');
+
+    initiContactInfoLabel(radios, contactInfoLabel);
+
+    radios.forEach((radio, index) => {
+        radio.addEventListener('change', () => {
+            if (radio.value === 'email') {
+                contactInfoLabel.innerText = 'Email';
+            } else if (radio.value === 'whatsapp') {
+                contactInfoLabel.innerText = 'WhatsApp';
+            }
+        })
+    });
+}
+
+export function initContactForm() {
+    toggleContactMethod();
+    toggleBudgetInput();
+    toggleContactInfoLabel();
+}
