065769b0ce180dcc3965664a90a852d2fd65ee5a
feeribeiro <fee.ribeiro.ribeiro96@gmail.com>
2025-05-06 22:34:23 -0300

    [UPDATE] ajustando reset e botão de envio de form

--- public/js/modules/support.js
+++ public/js/modules/support.js
@@ -1,8 +1,16 @@
-import { gsap } from "../libs/gsap-public/esm/gsap-core.js";
 import lottie from '../libs/lottie/lottie.min.js';
-
+import { gsap } from "../libs/gsap-public/esm/all.js";
+import { ScrollTrigger } from "../libs/gsap-public/esm/ScrollTrigger.js";
 
 export function setupSupport() {
+    lottie.loadAnimation({
+        container: document.getElementById('arrow'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: false,
+        path: 'public/assets/images/support/arrow.json'
+    });
+
     lottie.loadAnimation({
         container: document.getElementById('project'),
         renderer: 'svg',
@@ -12,17 +20,47 @@ export function setupSupport() {
     });
 
     lottie.loadAnimation({
-        container: document.getElementById('arrow'),
+        container: document.getElementById('rocket'),
         renderer: 'svg',
         loop: true,
-        autoplay: false,
-        path: 'public/assets/images/support/arrow.json'
+        autoplay: true,
+        path: 'public/assets/images/support/rocket.json'
     });
 
-    gsap.to(".arrow-line", {
-        width: "500px", // ou a largura desejada
-        duration: 2,
-        ease: "power2.out"
-    });
+    gsap.registerPlugin(ScrollTrigger);
+    ScrollTrigger.matchMedia({
+        // Para telas maiores (desktop e tablets)
+        "(min-width: 768px)": function () {
+            gsap.to('.services', {
+                xPercent: -50,
+                scrollTrigger: {
+                    trigger: '.services',
+                    start: "top top",
+                    end: "+=100%", // duração do scroll
+                    scrub: 1,
+                    pin: true,
+                    anticipatePin: 1,
+                    markers: false // coloque true se quiser ver os marcadores
+                }
+            });
+        },
 
+        // Para mobile (telas pequenas)
+        "(max-width: 767px)": function () {
+            // Aqui você pode desativar o efeito ou fazer uma animação mais simples
+            ScrollTrigger.create({
+                trigger: ".services",
+                start: "top top",
+                end: "+=100%",
+                scrub: true,
+                markers: false,
+                onUpdate: (self) => {
+                    // Exemplo de fade só pra não deixar em branco
+                    gsap.to('.services', {
+                        opacity: 1 - self.progress // só pra parecer que tá animando
+                    });
+                }
+            });
+        }
+    });
 }
