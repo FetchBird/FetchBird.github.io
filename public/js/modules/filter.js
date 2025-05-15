d71367469b6967ac58ea489b6204229ccac592e4
Rodrigues <julio.m.rodrigues@accenture.com>
2025-05-14 23:13:22 -0300

    filtro fase 1

--- public/js/modules/filter.js
+++ public/js/modules/filter.js
@@ -1,29 +1,36 @@
-const btn = document.querySelector('.cat');
-const listeFilter = document.querySelector('.filter');
-const filterItems = listeFilter.querySelectorAll('li');
-
 export function ativarFiltro() {
+  const btn = document.querySelector('.cat');
+  const listeFilter = document.querySelector('.filter');
+  const filterItems = listeFilter.querySelectorAll('li');
+  const filterOptionals = document.querySelector(".filterOptionals");
+  const filterOpt = document.querySelectorAll(".app");
+
   // Toggle para abrir/fechar o filtro
   btn.addEventListener('click', (e) => {
- 
-    listeFilter.classList.toggle('ativo');
+    filterOptionals.classList.toggle('ativo');
   });
-
-
 
   filterItems.forEach(li => {
     li.addEventListener('click', (e) => {
-      console.log('====================================');
-      console.log(e);
-      console.log('====================================');
       e.target.classList.toggle('ativo');
+
+      // Verifica se ainda existe algum li com a classe 'ativo'
+      const algumAtivo = Array.from(filterItems).some(item => item.classList.contains('ativo'));
+
+      filterOpt.forEach(el => {
+        if (algumAtivo) {
+          el.classList.add('ativo');
+        } else {
+          el.classList.remove('ativo');
+        }
+      });
     });
   });
 
   // Fecha filtro ao clicar fora
   document.addEventListener('click', function (e) {
-    if (!listeFilter.contains(e.target) && !btn.contains(e.target)) {
-      listeFilter.classList.remove('ativo');
+    if (!filterOptionals.contains(e.target) && !btn.contains(e.target)) {
+      filterOptionals.classList.remove('ativo');
     }
   });
 }
